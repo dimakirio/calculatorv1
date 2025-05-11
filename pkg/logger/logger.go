cf998b9a194739e67d00ced7d3e42365e46f909a
dimakirio <kirionenkodima@gmail.com>
2025-05-11 21:13:07 +0500

    Update logger.go

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,27 @@
+package logger
 
+import (
+	"log"
+	"os"
+)
+
+type Logger struct {
+	*log.Logger
+}
+
+func NewLogger(level string) *Logger {
+	return &Logger{log.New(os.Stdout, "", log.LstdFlags)}
+}
+
+func (l *Logger) Info(msg string) {
+	l.Println("INFO: " + msg)
+}
+
+func (l *Logger) Error(msg string) {
+	l.Println("ERROR: " + msg)
+}
+
+func (l *Logger) Fatal(msg string) {
+	l.Println("FATAL: " + msg)
+	os.Exit(1)
+}
